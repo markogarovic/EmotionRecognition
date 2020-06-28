1bdb396100714509459561a74672147553084a94
djuka7 <marko@djukanovic.me>
2020-06-29 01:02:39 +0200

    Commit yt

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
 const video = document.getElementById('video')
-
+let localStream;
+var isWebcamOn = 0;
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
-
-function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+]).then(app)
+function app(){
+  document.querySelector(".start").addEventListener("click", startVideo);
+  document.querySelector(".stop").addEventListener("click", stopVideo);
+}
+function stopVideo(){
+  localStream.getVideoTracks()[0].stop();
+  isWebcamOn = 0;
+}
+async function startVideo() {  
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia =
+      navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia ||
+      navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+        { video: true },
+        stream => {
+          localStream = stream;
+          isWebcamOn = 1;
+          video.srcObject = stream;
+          video.addEventListener("loadeddata", () => resolve(), false);
+        },
+        error => reject()
+      );
+    } else {
+      alert("Something wrong with webcam!");
+		  isWebcamOn = 0;
+      reject();
+    }
+  });
 }
 
 video.addEventListener('play', () => {
@@ -25,7 +52,7 @@ video.addEventListener('play', () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
