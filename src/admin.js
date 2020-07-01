d9c474ce27892cd1a6d93fb0201a96d55e5900f0
markogarovic <markogarovic1998@gmail.com>
2020-07-01 23:44:35 +0200

    'last'

--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,5 @@
 if(!localStorage.getItem("auth-token")){
-    window.location.replace("http://127.0.0.1:5500/html/login.html");
+    window.location.replace("login.html");
 }
 const logoutBtn = document.getElementById("logout");
 logoutBtn.addEventListener("click",(e)=>{
--- src/login.js
+++ src/login.js
@@ -40,7 +40,7 @@ loginBtn.addEventListener("click", (e)=>{
         return
     })
     .then(()=>{
-        window.location.replace("http://127.0.0.1:5500/html/homepage.html");
+        window.location.replace("homepage.html");
     })
 
     .catch((error) => {
